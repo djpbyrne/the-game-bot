7df357bf44e58c5a633a9358b82bd5b8b4f4f08f
Declan <declan.jp.byrne@gmail.com>
2022-06-21 00:41:41 -0700

    I just lost the game

---
+++ bot.py
@@ -0,0 +1,21 @@
+# Code based on sample from: https://realpython.com/how-to-make-a-discord-bot-python/
+
+import os
+import discord 
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+bot = commands.Bot(command_prefix='/')
+
+@bot.command(name='lost')
+async def lose_the_game(ctx):
+    user = ctx.author
+    await ctx.message.delete()
+    await ctx.send(f'@here, {user.mention} lost the game')
+
+bot.run(TOKEN)
